8b20481e7a8a070ea705ff036917bede62a9e385
kubota_syou <kirby8250@gmail.com>
2020-05-31 22:25:32 +0900

    マップ5追加 ちょっと重いかも

--- 3nen_GT_7/DXlib/DXlib/Scene/GamePlay.cpp
+++ 3nen_GT_7/DXlib/DXlib/Scene/GamePlay.cpp
@@ -112,7 +112,7 @@ void GamePlay::Init()
 	else if (mStageName == "stage5")
 	{
 		enemy->InitM("./Assets/Data/EnemyList5.csv");
-		//enemy->Init("");
+		enemy->Init("./Assets/Data/PatrolList2.csv");
 	}
 	else//—áŠOˆ—
 	{
